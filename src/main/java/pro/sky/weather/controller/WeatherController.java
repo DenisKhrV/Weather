cb2e61f9aafe7ed7d3dda1009595af46adaf0926
DenisKhrV <DenisKhr@yandex.ru>
2024-01-20 12:15:53 +1000

    Init

---
+++ src/main/java/pro/sky/weather/controller/WeatherController.java
@@ -0,0 +1,22 @@
+package pro.sky.weather.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.weather.model.Weather;
+import pro.sky.weather.service.WeatherService;
+
+@RestController
+public class WeatherController {
+
+    @Autowired
+    private WeatherService weatherService;
+
+    @GetMapping("/weather/{city}")
+    public ResponseEntity<Weather> getWeather(@PathVariable("city") String city) {
+        Weather weather = weatherService.getWeather(city);
+        return ResponseEntity.ok(weather);
+    }
+}
