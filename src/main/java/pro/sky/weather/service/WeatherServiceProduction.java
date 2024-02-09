1ab3e1de9ea4e471e6f68138c4e558f3c26f9bb3
DenisKhrV <DenisKhr@yandex.ru>
2024-02-09 17:59:47 +1000

    release
    (cherry picked from commit 21503b82cd92bf12ef3f19f92b935eeac0dcd3e0)

--- src/main/java/pro/sky/weather/service/WeatherServiceProduction.java
+++ src/main/java/pro/sky/weather/service/WeatherServiceProduction.java
@@ -41,6 +41,6 @@ public class WeatherServiceProduction implements WeatherService {
     }
 
     public void feature1() {
-        System.out.println("test");
+        System.out.println("release");
     }
 }
